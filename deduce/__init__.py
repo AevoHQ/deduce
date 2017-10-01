2fa1f4f0e582c173ce5089fd17e6893c4f28cf8c
Harrison Grodin <grodinh@winchesterthurston.org>
2017-09-30 21:58:50 -0400

    Add initial config setup

---
+++ deduce/__init__.py
@@ -0,0 +1,5 @@
+"""Aevo Deduce."""
+
+from .config import ARGS
+
+__all__ = ["ARGS"]
---
+++ deduce/config.py
@@ -0,0 +1,12 @@
+"""Aevo Deduce configuration."""
+
+import argparse
+
+
+def _build_parser():
+
+    parser = argparse.ArgumentParser()
+
+    return parser
+
+ARGS = _build_parser().parse_args()
